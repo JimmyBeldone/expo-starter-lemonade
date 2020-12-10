e1379f0dab0f89a3ce720a52f58247947a4dc2d0
Jimmy <jimmy.beldone@gmail.com>
2020-12-10 17:13:36 +0100

    chore: add eslint / prettier / pre-commit lint

---
+++ .eslintrc.js
@@ -0,0 +1,49 @@
+module.exports = {
+    env: {
+        browser: true,
+        node: true,
+    },
+
+    extends: [
+        'plugin:react/recommended',
+        'airbnb',
+        'airbnb/hooks',
+        'prettier',
+        'prettier/react',
+        'plugin:prettier/recommended',
+    ],
+    globals: {
+        __PATH_PREFIX__: true,
+    },
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 12,
+        sourceType: 'module',
+    },
+    // parser: 'babel-eslint',
+    plugins: ['prettier', 'sort-keys-fix', 'sort-destructure-keys', 'react'],
+    root: true,
+    rules: {
+        'implicit-arrow-linebreak': 0,
+        indent: [0, 4],
+        'jsx-quotes': ['error', 'prefer-single'],
+        'no-console': 0,
+        'object-curly-newline': 0,
+        'operator-linebreak': 0,
+        'prettier/prettier': 'error',
+        'react-hooks/exhaustive-deps': 'warn',
+        'react-hooks/rules-of-hooks': 'error',
+        'react/forbid-prop-types': 0,
+        'react/jsx-indent': 0,
+        'react/jsx-indent-props': 0,
+        'react/jsx-props-no-spreading': 0,
+        'react/no-unused-prop-types': 1,
+        'react/prop-types': 1,
+        'react/sort-prop-types': 1,
+        'react/style-prop-object': 0,
+        'sort-destructure-keys/sort-destructure-keys': 1,
+        'sort-keys-fix/sort-keys-fix': 'warn',
+    },
+};
