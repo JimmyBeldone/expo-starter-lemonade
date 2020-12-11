098128790c6ba25b55cd58a4327f08af01f7f711
Jimmy Beldone <dev.jimmy.beldone@gmail.com>
2020-12-11 16:50:08 +0100

    feat: ✨ Add colors constants

---
+++ src/constants/colors.js
@@ -0,0 +1,17 @@
+const colors = {
+    aquamarine: '#00dfae',
+    charcoalGrey: '#323c4a',
+    dark: '#222a35',
+    heliotrope: '#f44afe',
+    lightGreyBlue: '#b2b8c2',
+    lightLightBlue: '#d2fbf0',
+    paleGrey: '#f4f4f5',
+    slate: '#505e73',
+    slate20: 'rgba(80, 94, 115, 0.2)',
+    strongPink: '#ff006c',
+    sunflowerYellow: '#f7d800',
+    white: '#ffffff',
+    white10: 'rgba(255, 255, 255, 0.1)',
+};
+
+export default colors;
